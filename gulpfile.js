0ced0b81a3fedf64db3c3375aec74eb0959aa50b
n891331 <n891331@gmail.com>
2019-01-22 13:16:18 +0800

    add css/scss

--- gulpfile.js
+++ gulpfile.js
@@ -29,7 +29,7 @@ gulp.task('default', ['sass'], function () {
         server: {
             //根目錄
             baseDir: "./",
-            index: "index.html"
+            index: "nnnnn.html"
         }
     });
 
