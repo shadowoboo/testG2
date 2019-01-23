01790cd93b4fdc18e1bfbe420fb9f0ebe39c0608
rickjhang <whatup110119@gmail.com>
2019-01-24 01:16:34 +0800

    01.23新增修改(home)

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
-
-
 $(document).ready(function(){
-    $('.countryImg').animate({
-        top:"500px",
-    },3000,'easeOutBounce');
+    
+    $('.countryImg').each(function () {
+        var originTop = $(this).css('top');//先抓原本你ＣＳＳ寫的高度
+        $(this).css('top','-100px');//然後把國旗放到0的位置
+        $(this).animate({//做個動畫讓它們掉下來}
+            top:originTop,
+        }, 3000, 'easeOutBounce');
+    })
 });
+
