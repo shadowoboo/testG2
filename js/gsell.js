1bbe0850269aabf6087bfff64298f3611de3be72
n891331 <n891331@gmail.com>
2019-01-26 18:17:58 +0800

    修改檔案

--- js/gsell.js
+++ js/gsell.js
@@ -3,14 +3,12 @@
 function initMap() {
     // 下面的代碼構造了一個新的Google地圖對象，並向地圖添加了屬性，包括中心和縮放級別。
     
-      // The location of Uluru烏魯魯的位置
-      var taipei = {lat: 24.9650192, lng: 121.1909533};
+      // 位置
+      var uluru = {lat: 24.9650192, lng: 121.1909533};
       // The map, centered at Uluru地圖以烏魯魯為中心
       var map = new google.maps.Map(
-          document.getElementById('map'), {zoom: 4, center: taipei});
+          document.getElementById('map'), {zoom: 4, center: uluru});
       // The marker, positioned at Uluru標記，位於烏魯魯。下面的代碼在地圖上放置一個標記。
-      var marker = new google.maps.Marker({position: taipei, map: map});
+      var marker = new google.maps.Marker({position: uluru, map: map});
     }
       
-     
-//   剩下申請金鑰 
