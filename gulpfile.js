ac45cbdd4af8df2ea9d695a835d027ac76dcd18c
rickjhang <whatup110119@gmail.com>
2019-01-29 03:02:00 +0800

    0129解決沒有gulpfile的問題，並修改CSS

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+var jquery = require('gulp-jquery');
+
+// sass 編譯函式
+gulp.task('sass', function () {
+    return gulp.src('./sass/*.scss') //來源目錄
+        .pipe(sass().on('error', sass.logError)) //經由sass 轉譯
+        .pipe(gulp.dest('./css')); //目的地目錄
+});
+
+
+//jquery環境
+gulp.task('jquery', function () {
+    return gulp.src('./node_modules/jquery/src')
+        .pipe(jquery({
+            flags: ['-deprecated', '-event/alias', '-ajax/script', '-ajax/jsonp', '-exports/global']
+        }))
+        .pipe(gulp.dest('./public/vendor/'));
+    // creates ./public/vendor/jquery.custom.js
+});
+
+
+gulp.task('default', ['sass'], function () {
+
+    browserSync.init({
+        server: {
+            //根目錄
+            baseDir: "./",
+            index: "page/index.html"
+        }
+    });
+
+    gulp.watch(["sass/*.scss", "sass/**/*.scss"], ['sass']).on('change', reload);
+    gulp.watch("*.html").on('change', reload);
+    gulp.watch("page/*.html").on('change', reload);
+    gulp.watch("js/*.js").on('change', reload);
+    gulp.watch("images/*").on('change', reload);
+    // gulp.watch("images/*").on('change', reload);
+});
+
+
+gulp.task('css', ['cssUrls']);
